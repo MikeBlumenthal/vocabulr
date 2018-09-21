0cf8834f35bf88a792d424e8d157a407cdbe19f1
Michael Blumenthal <bloomer25@googlemail.com>
2018-09-21 12:50:18 +0100

    adds dependencies

---
+++ app/webpack.config.js
@@ -0,0 +1,9 @@
+const config = {
+  entry: `${__dirname}/client/src/app.js`,
+    path: `${__dirname}/client/public/js`, 
+    filename: 'bundle.js'
+  },
+  mode: 'development'
+};
+
+module.exports = config;
