61c5be1dc4b07dc6cd4be529c834e1bbdc53ae19
Ben <benharvie94@gmail.com>
2018-09-22 19:18:29 +0100

    Added correct t/f value to answer div+ created hint-view

--- app/client/src/views/answer_view.js
+++ app/client/src/views/answer_view.js
@@ -6,14 +6,19 @@ AnswerView.prototype.render = function (answerObj) {
   const answer = answerObj.answer;
   const img = answerObj.image;
   const correct = answerObj.correct;
+
   const div = document.createElement('div');
   div.classList.add('answer');
+  div.setAttribute('correct', correct);
+
   const answerEl = document.createElement('h4');
   answerEl.textContent = answer;
   div.appendChild(answerEl);
+
   const imageEl = document.createElement('img');
   imageEl.src = img;
   div.appendChild(imageEl);
+
   this.element.appendChild(div);
 };
 
