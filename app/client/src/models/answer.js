f5d5cc6aa121aae8191abc683a56761c91b9d209
Michael Blumenthal <bloomer25@googlemail.com>
2018-09-24 11:44:20 +0100

    sorts out error

---
+++ app/client/src/models/answer.js
@@ -0,0 +1,37 @@
+const PubSub = require('../helpers/pub_sub.js');
+const Request = require('../helpers/request.js');
+
+const Answer = function () {
+
+};
+
+Answer.prototype.bindEvents = function () {
+  PubSub.subscribe('ResponseView:answer-selected', (event) => {
+    if( event.detail.value !== undefined ){
+      this.check(event.detail);
+    };
+  });
+};
+
+Answer.prototype.check = function (answer) {
+  const userGuess = answer.value;
+  const request = new Request('http://localhost:3000/api/questions');
+  request.getOne(answer.id).then((response) => {
+    const correctAnswer = response[0].answers.find((answer) => {
+      return answer.correct === true;
+    });
+
+    const result = {};
+    if (userGuess === correctAnswer.answer) {
+      result.correct = answer;
+      console.log('Correct!');
+    } else {
+      result.incorrect = answer;
+      console.log('Incorrect :(');
+    };
+    PubSub.publish('Answer:answer-selected', result);
+  });
+};
+
+
+module.exports = Answer;
